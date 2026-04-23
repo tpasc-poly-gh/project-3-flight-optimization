dfc0251c834c97ab980fc1bf6f29f144a43628f7
Nathan J Sutjiono <nsutjiono0017@floridapoly.edu>
2026-04-23 20:29:54 +0000

    d

---
+++ WeightedGraph.hpp
@@ -0,0 +1,33 @@
+#ifndef WEIGHTEDGRAPH_HPP
+#define WEIGHTEDGRAPH_HPP
+#include <vector>
+
+
+template <typename T>
+class WeightedGraph {
+public:
+
+    void insertVertex(const T& v);
+    void insertEdge(const T& v1, const T& v2);
+    void print() const;
+
+    void DFS() const;
+    void BFS(int start = 0) const;
+    int shortestPath(const T& src, const T& dest) const;
+    bool isConnected() const;
+
+
+private:
+    std::vector<T> vertices; 
+    std::vector<std::vector<int>> edges; 
+
+    
+    int getVertexIndex(const T& vs) const;
+
+    bool hasEdge(int i1, int i2) const;
+    void DFS(int i, std::vector<bool>& visited) const;
+};
+
+#include "WeightedGraph.tpp"
+
+#endif
